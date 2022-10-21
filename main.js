50cbd359d43e51ae1e1abc57c33ecd3e22c1252c
Tim White <35063371+itstimwhite@users.noreply.github.com>
2022-10-21 11:37:26 -0700

    initial_twitch

--- main.js
+++ main.js
@@ -1,80 +1,53 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const Apify = require('apify');
+const Apify = require("apify");
 const { log, puppeteer } = Apify.utils;
-const utils = require('./src/utility');
-const handlePageFunction = require('./src/handlePageFunction');
+const utils = require("./src/utility");
+const handlePageFunction = require("./src/handlePageFunction");
 
 Apify.main(async () => {
-    const input = await Apify.getInput();
+  const input = await Apify.getInput();
 
-    if (!input)
-        throw 'Invalid input. Check apify_storage/key_value_stores/default/INPUT.json file.';
+  if (!input) throw "Invalid input. Check apify_storage/key_value_stores/default/INPUT.json file.";
 
-    const {
-        startUrls = [],
-        handlePageTimeoutSecs,
-        maxRequestRetries,
-        minConcurrency,
-        maxConcurrency,
-        maxRequestsPerCrawl,
-        proxyConfiguration
-    } = input;
+  const { startUrls = [], handlePageTimeoutSecs, maxRequestRetries, minConcurrency, maxConcurrency, maxRequestsPerCrawl, proxyConfiguration } = input;
 
-    if (process.env?.VERBOSE_LOG?.toLowerCase() === 'true') {
-        log.setLevel(log.LEVELS.DEBUG);
-    }
+  if (process.env?.VERBOSE_LOG?.toLowerCase() === "true") {
+    log.setLevel(log.LEVELS.DEBUG);
+  }
 
-    const requestList = await Apify.openRequestList(null, startUrls);
-    const requestQueue = await Apify.openRequestQueue();
+  const requestList = await Apify.openRequestList(null, startUrls);
+  const requestQueue = await Apify.openRequestQueue();
 
-    let req;
-    while (req = await requestList.fetchNextRequest()) {
-        await requestQueue.addRequest({ url: req.url += '/about' });
-    }
+  let req;
+  while ((req = await requestList.fetchNextRequest())) {
+    await requestQueue.addRequest({ url: (req.url += "/about") });
+  }
 
-    const proxyConfig = await utils.proxyConfiguration({
-        proxyConfig: proxyConfiguration,
-    });
+  const proxyConfig = await utils.proxyConfiguration({
+    proxyConfig: proxyConfiguration,
+  });
 
-    const crawler = new Apify.PuppeteerCrawler({
-        requestQueue,
-        handlePageFunction,
-        handlePageTimeoutSecs: handlePageTimeoutSecs || 30,
-        maxRequestRetries: maxRequestRetries || 1,
-        minConcurrency: minConcurrency || 1,
-        maxConcurrency: maxConcurrency || 100,
-        maxRequestsPerCrawl: maxRequestsPerCrawl || 10,
-        browserPoolOptions: { maxOpenPagesPerBrowser: 1 },
-        useSessionPool: true,
-        proxyConfiguration: proxyConfig,
-        preNavigationHooks: [
-            async ({ page }, gotoOptions) => {
-                await puppeteer.blockRequests(page, {
-                    urlPatterns: [
-                        '.mp4',
-                        '.webp',
-                        '.jpeg',
-                        '.jpg',
-                        '.gif',
-                        '.svg',
-                        '.ico',
-                        '.png',
-                        'google-analytics',
-                        'doubleclick.net',
-                        'googletagmanager',
-                        '/videoplayback',
-                        '/adview',
-                        '/stats/ads',
-                        '/stats/watchtime',
-                        '/stats/qoe',
-                        '/log_event',
-                    ],
-                });
-                gotoOptions.waitUntil = 'networkidle2';
-            }
-        ]
-    });
+  const crawler = new Apify.PuppeteerCrawler({
+    requestQueue,
+    handlePageFunction,
+    handlePageTimeoutSecs: handlePageTimeoutSecs || 30,
+    maxRequestRetries: maxRequestRetries || 1,
+    minConcurrency: minConcurrency || 1,
+    maxConcurrency: maxConcurrency || 100,
+    maxRequestsPerCrawl: maxRequestsPerCrawl || 10,
+    browserPoolOptions: { maxOpenPagesPerBrowser: 1 },
+    useSessionPool: true,
+    proxyConfiguration: proxyConfig,
+    preNavigationHooks: [
+      async ({ page }, gotoOptions) => {
+        await puppeteer.blockRequests(page, {
+          urlPatterns: [".mp4", ".webp", ".jpeg", ".jpg", ".gif", ".svg", ".ico", ".png", "google-analytics", "doubleclick.net", "googletagmanager", "/videoplayback", "/adview", "/stats/ads", "/stats/watchtime", "/stats/qoe", "/log_event"],
+        });
+        /* gotoOptions.waitUntil = "networkidle2"; */
+      },
+    ],
+  });
 
-    await crawler.run();
+  await crawler.run();
 });
--- src/handlePageFunction.js
+++ src/handlePageFunction.js
@@ -1,222 +1,205 @@
-const Apify = require('apify');
+const Apify = require("apify");
 const { log } = Apify.utils;
-const constants = require('./constants');
-const utils = require('./utility');
+const { social } = Apify.utils;
+const constants = require("./constants");
+const utils = require("./utility");
 
 module.exports = async ({ page, request, session, response }) => {
-    log.info(`Starting data extraction for ${request.url}`);
+  log.info(`Starting data extraction for ${request.url}`);
 
-    const captchaFrame = await page.frames().find(f => f.name().startsWith('a-'));
+  /* const captchaFrame = await page.frames().find(f => f.name().startsWith('a-'));
     if (captchaFrame) {
         const hasCaptcha = await captchaFrame.waitForSelector('div.recaptcha-checkbox-border');
         if (hasCaptcha) {
             session.retire();
             throw 'Got captcha, page will be retried. If this happens often, consider increasing number of proxies';
         }
-    }
+    } */
 
-    const statusCode = +response.status();
-    if (statusCode >= 400) {
-        session.retire();
-        throw `Response status is: ${response.status()} msg: ${response.statusText()}`;
-    }
+  /*  const statusCode = +response.status();
+  if (statusCode >= 400) {
+    session.retire();
+    throw `Response status is: ${response.status()} msg: ${response.statusText()}`;
+  }
 
-    if (await page.$('.yt-upsell-dialog-renderer')) {
-        await page.evaluate(async () => {
-            const noThanks = document.querySelectorAll('.yt-upsell-dialog-renderer [role="button"]');
+  if (await page.$(".yt-upsell-dialog-renderer")) {
+    await page.evaluate(async () => {
+      const noThanks = document.querySelectorAll('.yt-upsell-dialog-renderer [role="button"]');
 
-            for (const button of noThanks) {
-                if (button.textContent && button.textContent.includes('No thanks')) {
-                    button.click();
-                    break;
-                }
-            }
-        });
-    }
-
-    if (page.url().includes('consent')) {
-        log.debug('Clicking consent dialog');
-
-        await Promise.all([
-            page.$eval('form[action*="consent"]', (el) => {
-                el.querySelector('button')?.click();
-            }),
-            page.waitForNavigation({ waitUntil: 'networkidle2' }),
-        ]);
-
-        session.retire();
-    }
-
-    const {
-        CHANNEL_NAME_XP,
-        CHANNEL_SUBSCRIBER_COUNT_XP,
-        JOINED_DATE_XP,
-        TOTAL_VIEW_COUNT_XP,
-        CHANNEL_LOCATION_XP,
-        CHANNEL_DESCRIPTION_XP,
-        CHANNEL_PROFILE_IMAGE_XP,
-    } = constants.SELECTORS_XP;
-
-    const channelName = await utils.getDataFromXpath(page, CHANNEL_NAME_XP, 'innerHTML')
-        .catch((e) => utils.handleErrorAndScreenshot(page, e, 'Getting-channelName-failed'));
-    log.debug(`Got channelName as ${channelName}`);
-
-    const channelSubscriberCount = await utils.getDataFromXpath(page, CHANNEL_SUBSCRIBER_COUNT_XP, 'innerHTML')
-        .then(str => utils.unformatNumbers(str))
-        .catch((e) => utils.handleErrorAndScreenshot(page, e, 'Getting-channelSubscriberCount-failed'));
-    log.debug(`Got channelSubscriberCount as ${channelSubscriberCount}`);
-
-    const totalViewCount = await utils.getDataFromXpath(page, TOTAL_VIEW_COUNT_XP, 'innerHTML')
-        .then(str => utils.unformatNumbers(str))
-        .catch((e) => utils.handleErrorAndScreenshot(page, e, 'Getting-totalViewCount-failed'));
-    log.debug(`Got totalViewCountStr as ${totalViewCount}`);
-
-    const joinedDate = await utils.getDataFromXpath(page, JOINED_DATE_XP, 'innerHTML')
-        .catch((e) => utils.handleErrorAndScreenshot(page, e, 'Getting-joinedDate-failed'));
-    log.debug(`Got joinedDate as ${joinedDate}`);
-
-    /*
-    * @TODO This is a fragile way to get the location, since if another attribute is added to the details section,
-    *   it will broke. A better approach is to get a array of attribute name / value.
-    * */
-    const channelLocation = await utils.getDataFromXpath(page, CHANNEL_LOCATION_XP, 'innerHTML')
-        .catch((e) => utils.handleErrorAndScreenshot(page, e, 'Getting-channelLocation-failed'));
-    log.debug(`Got channelLocation as ${channelLocation}`);
-
-    const channelDescription = await utils.getDataFromXpath(page, CHANNEL_DESCRIPTION_XP, 'innerHTML')
-        .catch((e) => utils.handleErrorAndScreenshot(page, e, 'Getting-channelDescription-failed'));
-    log.debug(`Got channelDescription as ${channelDescription}`);
-
-    const channelProfileImageURL = await utils.getDataFromXpath(page, CHANNEL_PROFILE_IMAGE_XP, 'src')
-        .catch((e) => utils.handleErrorAndScreenshot(page, e, 'Getting-channelProfileImage-failed'));
-    log.debug(`Got channelProfileImage as ${channelProfileImageURL}`);
-
-    const allUrls = await page.evaluate(() => {
-        const anchors = Array.from(document.querySelectorAll('a'));
-        return anchors.map(anchor => anchor.href);
-    });
-
-    // Search for 'q' in the url in the array allUrls and output it to redirectUrls2
-    const redirectUrls2 = allUrls.filter(url => url.includes('q='));
-    if (redirectUrls2) log.debug(`Got redirectUrls2 as ${redirectUrls2.join(',')}`);
-
-    // Get the value of the 'q' parameter from the url in the array redirectUrls2
-    const redirectUrls3 = redirectUrls2.map(url => url.split('q=')[1]);
-    if (redirectUrls3) log.debug(`Got redirectUrls3 as ${redirectUrls3.join(',')}`);
-
-    // Decode the urls in the array redirectUrls3 and output it to redirectUrls4
-    const redirectUrls4 = redirectUrls3.map(url => decodeURIComponent(url));
-    if (redirectUrls4) log.debug(`Got redirectUrls4 as ${redirectUrls4.join(',')}`);
-
-    // Dedupe the urls in the array redirectUrls4 and output it to redirectUrls5
-    const redirectUrls5 = Array.from(new Set(redirectUrls4));
-    if (redirectUrls5) log.debug(`Got redirectUrls5 as ${redirectUrls5.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'youtube' put it in a new array called youtubeUrls else put it in a new array called otherUrls
-    const youtubeUrls = redirectUrls5.filter(url => url.includes('youtube.com'));
-    if (youtubeUrls) log.debug(`Got youtubeUrls as ${youtubeUrls.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'instagram' put it in a new array called instagramUrls
-    const instagramUrls = redirectUrls5.filter(url => url.includes('instagram.com'));
-    if (instagramUrls) log.debug(`Got instagramUrls as ${instagramUrls.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'twitter' put it in a new array called twitterUrls
-    const twitterUrls = redirectUrls5.filter(url => url.includes('twitter.com'));
-    if (twitterUrls) log.debug(`Got twitterUrls as ${twitterUrls.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'facebook' put it in a new array called facebookUrls
-    const facebookUrls = redirectUrls5.filter(url => url.includes('facebook.com' || 'fb.com'));
-    if (facebookUrls) log.debug(`Got facebookUrls as ${facebookUrls.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'linkedin' put it in a new array called linkedinUrls
-    const linkedinUrls = redirectUrls5.filter(url => url.includes('linkedin.com'));
-    if (linkedinUrls) log.debug(`Got linkedinUrls as ${linkedinUrls.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'pinterest' put it in a new array called pinterestUrls
-    const pinterestUrls = redirectUrls5.filter(url => url.includes('pinterest.com'));
-    if (pinterestUrls) log.debug(`Got pinterestUrls as ${pinterestUrls.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'reddit' put it in a new array called redditUrls
-    const redditUrls = redirectUrls5.filter(url => url.includes('reddit.com'));
-    if (redditUrls) log.debug(`Got redditUrls as ${redditUrls.join(',')}`);
-
-    // If a url in the array redirectUrls5 contains 'tumblr' put it in a new array called tumblrUrls
-    const tumblrUrls = redirectUrls5.filter(url => url.includes('tumblr.com'));
-    if (tumblrUrls) log.debug(`Got tumblrUrls as ${tumblrUrls.join(',')}`);
-
-    // If a url in the array contains 'tiktok' put it in a new array called tiktokUrls
-    const tiktokUrls = redirectUrls5.filter(url => url.includes('tiktok.com'));
-    if (tiktokUrls) log.debug(`Got tiktokUrls as ${tiktokUrls.join(',')}`);
-
-    // If a url in the array contains 'tiktok' trim the url to remove the '?*" part and put it in a new array called tiktokUrls2"
-    const tiktokUrls2 = tiktokUrls.map(url => url.split('?')[0]);
-    if (tiktokUrls2) log.debug(`Got tiktokUrls2 as ${tiktokUrls2.join(',')}`);
-
-    // If a url in the array contains 'twitch' put it in a new array called twitchUrls
-    const twitchUrls = redirectUrls5.filter(url => url.includes('twitch'));
-    if (twitchUrls) log.debug(`Got twitchUrls as ${twitchUrls.join(',')}`);
-
-    // If a url in the array contains 'onlyfans' trim put it in a new array called onlyfansUrls
-    const onlyfansUrls = redirectUrls5.filter(url => url.includes('onlyfans'));
-    if (onlyfansUrls) log.debug(`Got onlyfansUrls as ${onlyfansUrls.join(',')}`);
-
-    // If a url in the array contains 'spotify' and ('user' or 'artist') put it in a new array called spotifyUrls
-    const spotifyUrls = redirectUrls5.filter(url => url.includes('spotify') && (url.includes('user') || url.includes('artist')));
-    if (spotifyUrls) log.debug(`Got spotifyUrls as ${spotifyUrls.join(',')}`);
-
-    // If a url in the array contains 'soundcloud' put it in a new array called soundcloudUrls
-    const soundcloudUrls = redirectUrls5.filter(url => url.includes('soundcloud'));
-    if (soundcloudUrls) log.debug(`Got soundcloudUrls as ${soundcloudUrls.join(',')}`);
-
-    /*  Look for 'xpathCategory' on the page. If not found, set category to null
-     *  If found, get the textContent of the element and set it as category
-     */
-    const verifiedCategory = await page.evaluate(() => {
-        const xpathCategory = document.evaluate('//*[@id="header"]/div[2]/div[2]/div/div[1]/div/div[1]/ytd-channel-name/ytd-badge-supported-renderer/div/tp-yt-paper-tooltip/div', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
-        if (xpathCategory === null) {
-            return null;
-        } else {
-            return xpathCategory.textContent;
+      for (const button of noThanks) {
+        if (button.textContent && button.textContent.includes("No thanks")) {
+          button.click();
+          break;
         }
+      }
     });
-    log.debug(`Got verifiedCategory as ${verifiedCategory}`);
+  }
 
+  if (page.url().includes("consent")) {
+    log.debug("Clicking consent dialog");
 
-    const channelURL = request.url.replace('/about', '');
+    await Promise.all([
+      page.$eval('form[action*="consent"]', (el) => {
+        el.querySelector("button")?.click();
+      }),
+      page.waitForNavigation({ waitUntil: "networkidle2" }),
+    ]);
 
-    const channelEmail = Apify.utils.social.emailsFromText(channelDescription);
-    log.debug(`Got email as ${channelEmail}`);
+    session.retire();
+  } */
 
-    const channelPhone = Apify.utils.social.phonesFromText(channelDescription);
-    log.debug(`Got phone as ${channelPhone}`);
+  const { CHANNEL_NAME_XP, CHANNEL_LINK1_XP, CHANNEL_LINK2_XP, CHANNEL_PANELS_XP, CHANNEL_LINK3_XP, CHANNEL_LINK4_XP, CHANNEL_LINK5_XP, CHANNEL_SUBSCRIBER_COUNT_XP, CHANNEL_DESCRIPTION_XP, CHANNEL_PROFILE_IMAGE_XP } = constants.SELECTORS_XP;
 
-    log.debug('Pushing item to dataset');
-    await Apify.pushData({
-        channelURL,
-        channelName,
-        channelEmail,
-        channelPhone,
-        channelSubscriberCount,
-        joinedDate,
-        totalViewCount,
-        channelLocation,
-        channelDescription,
-        channelProfileImageURL,
-        youtubeUrls,
-        instagramUrls,
-        twitterUrls,
-        facebookUrls,
-        linkedinUrls,
-        pinterestUrls,
-        redditUrls,
-        tumblrUrls,
-        tiktokUrls,
-        twitchUrls,
-        onlyfansUrls,
-        spotifyUrls,
-        soundcloudUrls,
-        verifiedCategory
-    });
+  var channelName = "";
+  var channelDescription = "";
+  var channelProfileImage = "";
+  var channelSubscriberCount = "";
+  var channelLink1 = "";
+  var channelLink2 = "";
+  var channelLink3 = "";
+  var channelLink4 = "";
+  var channelLink5 = "";
+  var youtubeUrls = [];
+  var instagramUrls = [];
+  var facebookUrls = [];
+  var twitterUrls = [];
+  var tiktokUrls = [];
+  var twitchUrls = [];
+  var snapchatUrls = [];
+  var pinterestUrls = [];
+  var redditUrls = [];
+  var discordUrls = [];
+  var onlyfansUrls = [];
+  var tumblrUrls = [];
+  var patreonUrls = [];
+  var spotifyUrls = [];
+  var soundcloudUrls = [];
+  var linkedinUrls = [];
+  var verifiedCategory = "";
 
-    log.info(`Finished data extraction for ${request.url}`);
+  var channelName = await utils.getDataFromXpath(page, CHANNEL_NAME_XP, "innerHTML").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelName-failed"));
+  log.debug(`Got channelName as ${channelName}`);
+
+  try {
+    var channelSubscriberCount = await utils
+      .getDataFromXpath(page, CHANNEL_SUBSCRIBER_COUNT_XP, "innerText")
+      .then((str) => utils.unformatNumbers(str))
+      .catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelSubscriberCount-failed"));
+    log.debug(`Got channelSubscriberCount as ${channelSubscriberCount}`);
+  } catch (e) {
+    log.debug(`Got channelSubscriberCount as ${channelSubscriberCount}`);
+  }
+
+  try {
+    var channelLink1 = await utils.getDataFromXpath(page, CHANNEL_LINK1_XP, "href").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelLink1-failed"));
+    log.debug(`Got channelLink1 as ${channelLink1}`);
+  } catch (e) {
+    log.debug("No channelLink1 found");
+  }
+
+  try {
+    var channelLink2 = await utils.getDataFromXpath(page, CHANNEL_LINK2_XP, "href").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelLink2-failed"));
+    log.debug(`Got channelLink2 as ${channelLink2}`);
+  } catch (e) {
+    log.debug("No channelLink2 found");
+  }
+
+  try {
+    var channelLink3 = await utils.getDataFromXpath(page, CHANNEL_LINK3_XP, "href").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelLink3-failed"));
+    log.debug(`Got channelLink3 as ${channelLink3}`);
+  } catch (e) {
+    log.debug("No channelLink3 found");
+  }
+
+  try {
+    var channelLink4 = await utils.getDataFromXpath(page, CHANNEL_LINK4_XP, "href").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelLink4-failed"));
+    log.debug(`Got channelLink4 as ${channelLink4}`);
+  } catch (e) {
+    log.debug("No channelLink4 found");
+  }
+
+  try {
+    var channelLink5 = await utils.getDataFromXpath(page, CHANNEL_LINK5_XP, "href").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelLink5-failed"));
+    log.debug(`Got channelLink5 as ${channelLink5}`);
+  } catch (e) {
+    log.debug("No channelLink5 found");
+  }
+
+  try {
+    var channelDescription = await utils.getDataFromXpath(page, CHANNEL_DESCRIPTION_XP, "innerHTML").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelDescription-failed"));
+    log.debug(`Got channelDescription as ${channelDescription}`);
+  } catch (e) {
+    log.debug("No channelDescription found");
+  }
+
+  try {
+    var channelPanels = await utils.getDataFromXpath(page, CHANNEL_PANELS_XP, "innerHTML").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelPanels-failed"));
+    log.debug(`Got channelPanels as ${channelPanels}`);
+  } catch (e) {
+    log.debug("No channelPanels found");
+  }
+  /* 
+  //add channelLink1, channelLink2, channelLink3, channelLink4, channelLink5, to an array called channelLinks
+  var channelLinks = await [channelLink1.href, channelLink2.href, channelLink3, channelLink4, channelLink5];
+ */
+  //find social links in channelLinks array
+
+  var channelProfileImageURL = await utils.getDataFromXpath(page, CHANNEL_PROFILE_IMAGE_XP, "src").catch((e) => utils.handleErrorAndScreenshot(page, e, "Getting-channelProfileImage-failed"));
+  log.debug(`Got channelProfileImage as ${channelProfileImageURL}`);
+
+  var allUrls = await page.evaluate(() => {
+    const anchors = Array.from(document.querySelectorAll("a"));
+    return anchors.map((anchor) => anchor.href);
+  });
+
+  try {
+    var twitchUrls = Apify.utils.social.getTwitchUrls(channelLinks);
+    log.debug(`Got twitchUrls as ${twitchUrls}`);
+  } catch (e) {
+    log.debug("No twitchUrls found");
+  }
+
+  var verifiedCategory = await page.evaluate(() => {
+    var xpathCategory = document.evaluate('//*[@aria-label="Verified Partner"]', document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+    if (xpathCategory === null) {
+      return null;
+    } else {
+      return "Verified Partner";
+    }
+  });
+  log.debug(`Got verifiedCategory as ${verifiedCategory}`);
+
+  var channelURL = request.url.replace("/about", "");
+
+  //search channelPanels for an email using APify.utilssocial.emailsFromText
+
+  var channelPanelsText = await page.evaluate((channelPanels) => channelPanels.textContent, channelPanels);
+  log.debug(`Got channelPanelsText as ${channelPanelsText}`);
+
+  var emails = await Apify.utils.social.emailsFromText(channelPanelsText);
+
+  log.debug(`Got emails as ${emails.join(",")}`);
+
+  var channelEmail = Apify.utils.social.emailsFromText(channelPanels);
+  log.debug(`Got email as ${channelEmail}`);
+
+  var channelPhone = Apify.utils.social.phonesFromText(channelPanels);
+  log.debug(`Got phone as ${channelPhone}`);
+
+  const socialHandles = social.parseHandlesFromHtml(channelPanels || channelLink1 || channelLink2 || channelLink3 || channelLink4 || channelLink5);
+  log.debug("Social handles: ", socialHandles);
+
+  log.debug("Pushing item to dataset");
+
+  await Apify.pushData({
+    channelURL,
+    channelName,
+    channelSubscriberCount,
+    channelDescription,
+    channelProfileImageURL,
+    socialHandles,
+    verifiedCategory,
+  });
+
+  log.info(`Finished data extraction for ${request.url}`);
 };
